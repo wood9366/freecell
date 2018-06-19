6ad27762f2b417274708e13e4c669fe51519e6cd
Li Yang <7325560@qq.com>
2018-06-19 12:58:26 +0800

    add state machine code framework.

---
+++ Assets/scripts/share/State.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State {
+    public virtual void enter() { }
+    public virtual void update(float deltaTime) { }
+    public virtual void exit() { }
+}
---
+++ Assets/scripts/share/StateMachine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : State {
+
+    public void setCurrentState(int id) {
+        if (_states.ContainsKey(id)) {
+            _curState = _states[id];
+        }
+    }
+
+    public void addState(int id, State state) {
+        _states.Add(id, state);
+    }
+
+    public override void update(float deltaTime) {
+        if (_curState != null) {
+            _curState.update(deltaTime);
+        }
+    }
+
+    State _curState = null;
+    Dictionary<int, State> _states = new Dictionary<int, State>();
+}
